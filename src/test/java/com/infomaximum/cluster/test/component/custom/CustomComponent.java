9f3e7f72b4aa96fc42bc66c3fa00ff37dbc1c256
Vladimir A. Ulitin <ulitin@infomaximum.com>
2021-09-01 16:12:31 +0300

    fix

--- src/test/java/com/infomaximum/cluster/test/component/custom/CustomComponent.java
+++ src/test/java/com/infomaximum/cluster/test/component/custom/CustomComponent.java
@@ -1,12 +1,13 @@
 package com.infomaximum.cluster.test.component.custom;
 
 import com.infomaximum.cluster.Cluster;
+import com.infomaximum.cluster.anotation.Info;
 import com.infomaximum.cluster.struct.Component;
 
 /**
  * Created by kris on 12.09.17.
  */
-@com.infomaximum.cluster.anotation.Component(uuid = "com.infomaximum.cluster.test.component.custom")
+@Info(uuid = "com.infomaximum.cluster.test.component.custom")
 public class CustomComponent extends Component {
 
     public CustomComponent(Cluster cluster) {
