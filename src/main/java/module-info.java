cd960cb7cf2eca66c0a00ce904b9f9d3fb1516d8
Vladimir A. Ulitin <ulitin@infomaximum.com>
2022-11-10 15:12:13 +0300

    fix: jdk 17

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.infomaximum.cluster.grpc {
     requires java.annotation;
     requires com.google.protobuf;
     requires com.google.common;
+    requires com.google.guava.failureaccess;
 
     exports com.infomaximum.cluster.core.service.transport.network.grpc;
     exports com.infomaximum.cluster.core.service.transport.network.grpc.struct;
