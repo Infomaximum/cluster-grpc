8175884ac7a0a451dd7947ef4ee09b2766915965
Vladimir A. Ulitin <v.ulitin@mobin.me>
2024-05-07 10:32:36 +0300

    fix: crash

--- src/main/java/com/infomaximum/cluster/core/service/transport/network/grpc/internal/channel/ChannelServer.java
+++ src/main/java/com/infomaximum/cluster/core/service/transport/network/grpc/internal/channel/ChannelServer.java
@@ -1,15 +1,20 @@
 package com.infomaximum.cluster.core.service.transport.network.grpc.internal.channel;
 
+import com.infomaximum.cluster.core.service.transport.network.grpc.internal.channel.client.Client;
 import com.infomaximum.cluster.core.service.transport.network.grpc.internal.struct.RNode;
 import com.infomaximum.cluster.core.service.transport.network.grpc.internal.utils.convert.ConvertProto;
 import com.infomaximum.cluster.core.service.transport.network.grpc.struct.PNetPackage;
 import com.infomaximum.cluster.core.service.transport.network.grpc.struct.PNetPackageHandshakeRequest;
 import io.grpc.stub.ServerCallStreamObserver;
 import io.grpc.stub.StreamObserver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.UUID;
 
 public class ChannelServer extends ChannelImpl {
+
+    private final static Logger log = LoggerFactory.getLogger(ChannelServer.class);
 
     ChannelServer(UUID uuid, RNode remoteNode, StreamObserver<PNetPackage> requestObserver) {
         super(uuid, remoteNode, requestObserver);
@@ -28,7 +33,12 @@ public class ChannelServer extends ChannelImpl {
     public void kill(Throwable throwable) {
         destroy();
         ServerCallStreamObserver<PNetPackage> serverCallStreamObserver = (ServerCallStreamObserver<PNetPackage>) requestObserver;
-        serverCallStreamObserver.onError(throwable);
+        try {
+            serverCallStreamObserver.onError(throwable);
+        } catch (IllegalStateException e) {
+            //Возможно канал уже успели закрыть, тогда сгенерируется ошибка: java.lang.IllegalStateException: call already closed
+            log.error("Exception kill channel", e);
+        }
     }
 
     @Override
