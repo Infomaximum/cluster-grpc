124a4585b134d398d9b597e7b826bd2de1e17848
vnaumkin <naumkin.vlad2012@yandex.ru>
2024-07-09 09:48:39 +0300

    feat: add Version in PNetPackageComponent
    Task: PT-14780

--- src/main/java/com/infomaximum/cluster/core/service/transport/network/grpc/internal/netpackage/NetPackageHandshakeCreator.java
+++ src/main/java/com/infomaximum/cluster/core/service/transport/network/grpc/internal/netpackage/NetPackageHandshakeCreator.java
@@ -1,14 +1,11 @@
 package com.infomaximum.cluster.core.service.transport.network.grpc.internal.netpackage;
 
-import com.infomaximum.cluster.Cluster;
 import com.infomaximum.cluster.core.component.RuntimeComponentInfo;
 import com.infomaximum.cluster.core.service.transport.network.grpc.internal.GrpcNetworkTransitImpl;
 import com.infomaximum.cluster.core.service.transport.network.grpc.struct.*;
 import com.infomaximum.cluster.core.service.transport.network.local.LocalManagerRuntimeComponent;
-import com.infomaximum.cluster.struct.Component;
 
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 public class NetPackageHandshakeCreator {
 
@@ -39,37 +36,30 @@ public class NetPackageHandshakeCreator {
                 .setRuntimeIdMostSigBits(nodeRuntimeId.getMostSignificantBits())
                 .setRuntimeIdLeastSigBits(nodeRuntimeId.getLeastSignificantBits());
 
-        Cluster cluster = grpcNetworkTransit.transportManager.cluster;
-        for(Component component: cluster.getLocalComponents()) {
-            nodeBuilder.addPNetPackageComponents(buildPackageComponent(component));
+        LocalManagerRuntimeComponent localManagerRuntimeComponent = grpcNetworkTransit.getManagerRuntimeComponent().getLocalManagerRuntimeComponent();
+        for (RuntimeComponentInfo runtimeComponentInfo : localManagerRuntimeComponent.getComponents()) {
+            nodeBuilder.addPNetPackageComponents(buildPackageComponent(runtimeComponentInfo));
         }
 
         return nodeBuilder.build();
     }
 
-    public static PNetPackageComponent buildPackageComponent(Component component) {
-        return PNetPackageComponent.newBuilder()
-                .setUuid(component.getInfo().getUuid())
-                .setId(component.getId())
-                .addAllClassNameRControllers(
-                        component.getTransport().getExecutor().getClassRControllers().stream().map(it -> it.getName()).collect(Collectors.toList())
-                )
-                .build();
-    }
-
     public static PNetPackage buildPacketUpdateNode(LocalManagerRuntimeComponent localManagerRuntimeComponent) {
         PNetPackageUpdateNode.Builder updateBuilder = PNetPackageUpdateNode.newBuilder();
         for (RuntimeComponentInfo runtimeComponentInfo : localManagerRuntimeComponent.getComponents()) {
-            updateBuilder.addPNetPackageComponents(NetPackageHandshakeCreator.buildPackageComponent(runtimeComponentInfo));
+            updateBuilder.addPNetPackageComponents(buildPackageComponent(runtimeComponentInfo));
         }
         return PNetPackage.newBuilder().setUpdateNode(updateBuilder).build();
     }
 
     public static PNetPackageComponent buildPackageComponent(RuntimeComponentInfo runtimeComponentInfo) {
-        return PNetPackageComponent.newBuilder()
+        PNetPackageComponent.Builder builder = PNetPackageComponent.newBuilder()
                 .setUuid(runtimeComponentInfo.uuid)
                 .setId(runtimeComponentInfo.id)
-                .addAllClassNameRControllers(runtimeComponentInfo.getClassNameRControllers())
-                .build();
+                .addAllClassNameRControllers(runtimeComponentInfo.getClassNameRControllers());
+        if (runtimeComponentInfo.version != null) {
+            builder.setVersion(runtimeComponentInfo.version.toString());
+        }
+        return builder.build();
     }
 }
--- src/main/java/com/infomaximum/cluster/core/service/transport/network/grpc/internal/utils/convert/ConvertProto.java
+++ src/main/java/com/infomaximum/cluster/core/service/transport/network/grpc/internal/utils/convert/ConvertProto.java
@@ -9,6 +9,7 @@ import com.infomaximum.cluster.core.service.transport.network.grpc.internal.stru
 import com.infomaximum.cluster.core.service.transport.network.grpc.struct.PNetPackageComponent;
 import com.infomaximum.cluster.core.service.transport.network.grpc.struct.PNetPackageHandshakeNode;
 import com.infomaximum.cluster.core.service.transport.network.grpc.struct.PNetPackageUpdateNode;
+import com.infomaximum.cluster.struct.Version;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,7 +22,7 @@ public class ConvertProto {
 
     private final static Logger log = LoggerFactory.getLogger(ConvertProto.class);
 
-    public static RNode convert(PNetPackageHandshakeNode handshakeNode){
+    public static RNode convert(PNetPackageHandshakeNode handshakeNode) {
         UUID nodeRuntimeId = new UUID(handshakeNode.getRuntimeIdMostSigBits(), handshakeNode.getRuntimeIdLeastSigBits());
 
         Node node = new GrpcNode.Builder(handshakeNode.getName(), nodeRuntimeId).build();
@@ -43,9 +44,10 @@ public class ConvertProto {
         return components;
     }
 
-    private static LocationRuntimeComponent convert(UUID nodeRuntimeId, PNetPackageComponent netComponent){
+    private static LocationRuntimeComponent convert(UUID nodeRuntimeId, PNetPackageComponent netComponent) {
         String uuid = netComponent.getUuid();
         int id = netComponent.getId();
+        Version version = "".equals(netComponent.getVersion()) ? null : Version.parse(netComponent.getVersion());
 
         HashSet<Class<? extends RController>> classRControllers = new HashSet<>();
         for (int i = 0; i < netComponent.getClassNameRControllersCount(); i++) {
@@ -58,6 +60,6 @@ public class ConvertProto {
                 log.debug("Unknown controller: {} in component: {}", classNameRController, uuid);
             }
         }
-        return new LocationRuntimeComponent(nodeRuntimeId, new RuntimeComponentInfo(id, uuid, classRControllers));
+        return new LocationRuntimeComponent(nodeRuntimeId, new RuntimeComponentInfo(id, uuid, version, classRControllers));
     }
 }
